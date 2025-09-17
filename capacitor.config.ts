5352eaaaa9999ce092171152884aef6fae383977
baribiamine-droid <baribiamine@gmail.com>
2025-09-17 13:41:45 -0700

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tondomaine.expressions',
+  appName: 'English Expressions',
+  webDir: 'public',
+  server: { androidScheme: 'https' }
+};
+
+export default config;
